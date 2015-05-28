3ce438b606aa2955cf25832d8e96850ff16d7fed
Mark Drei <github@mopedoversum.de>
2015-05-28 20:12:55 +0200

    Code grows slowly as I play around with PerfScope...

--- main.cpp
+++ main.cpp
@@ -5,17 +5,50 @@
  *      Author: mark
  */
 
-void unfrequentFunction() {
-	// does nothing
+void unfrequentFunctionLevel6() {
+	// no op
+}
+
+void unfrequentFunctionLevel5(int level) {
+	while (level > 5) {
+		unfrequentFunctionLevel6();
+		--level;
+	}
+}
+
+void unfrequentFunctionLevel4(int level) {
+	if (level > 4) {
+		unfrequentFunctionLevel5(level);
+	}
+}
+
+void unfrequentFunctionLevel3(int level) {
+	if (level > 3) {
+		unfrequentFunctionLevel4(level);
+	}
+}
+
+void unfrequentFunctionLevel2(int level) {
+	if (level > 2) {
+		unfrequentFunctionLevel3(level);
+	}
+}
+
+void unfrequentFunctionLevel1(int level) {
+	if (level > 1) {
+		unfrequentFunctionLevel2(level);
+	}
+}
+
+void unusedFunction() {
 }
 
 void frequentFunction() {
 	// does nothing
 }
 
-int main()
-{
-	unfrequentFunction();
+int main() {
+	unfrequentFunctionLevel1(4);
 
 	for (int i = 0; i < 100; ++i) {
 		frequentFunction();
