a8f23e62a3c0bbd58c28d7147dd5e90b0303a1f1
Mark Drei <github@mopedoversum.de>
2015-05-29 16:05:43 +0200

    added namespace hoping to create a scope that way

--- src/main.cpp
+++ src/main.cpp
@@ -7,60 +7,66 @@
 //#include <iostream>
 #include <unistd.h>
 
-void unfrequentFunctionLevel6() {
-	// no op
-}
+namespace mdd {
 
-void unfrequentFunctionLevel5(int level) {
-	while (level > 5) {
-		unfrequentFunctionLevel6();
-		--level;
+	void unfrequentFunctionLevel6() {
+		// no op
 	}
-}
 
-void unfrequentFunctionLevel4(int level) {
-	if (level > 4) {
-		unfrequentFunctionLevel5(level);
+	void unfrequentFunctionLevel5(int level) {
+		while (level > 5) {
+			unfrequentFunctionLevel6();
+			--level;
+		}
 	}
-}
 
-void expensiveFuntion() {
-//	cout << "Zzzzzz...." << endl;
-	usleep(500);
-//	cout << "Zzzzzz...." << endl;
-}
-
-void unfrequentFunctionLevel3(int level) {
-	if (level > 3) {
-		unfrequentFunctionLevel4(level);
+	void unfrequentFunctionLevel4(int level) {
+		if (level > 4) {
+			unfrequentFunctionLevel5(level);
+		}
 	}
-}
 
-void unfrequentFunctionLevel2(int level) {
-	if (level > 2) {
-		unfrequentFunctionLevel3(level);
+	void expensiveFuntion() {
+		//	cout << "Zzzzzz...." << endl;
+		//	usleep(500);
+		//	cout << "Zzzzzz...." << endl;
 	}
-}
 
-void unfrequentFunctionLevel1(int level) {
-	if (level > 1) {
-		unfrequentFunctionLevel2(level);
+	void unfrequentFunctionLevel3(int level) {
+		expensiveFuntion();
+
+		if (level > 3) {
+			unfrequentFunctionLevel4(level);
+		}
 	}
-}
 
-void unusedFunction() {
-}
-
-void frequentFunction() {
-	// does nothing
-}
-
-int main() {
-	unfrequentFunctionLevel1(4);
-
-	for (int i = 0; i < 100; ++i) {
-		frequentFunction();
+	void unfrequentFunctionLevel2(int level) {
+		if (level > 2) {
+			unfrequentFunctionLevel3(level);
+		}
 	}
-	return 0;
-}
 
+	void unfrequentFunctionLevel1(int level) {
+		expensiveFuntion();
+		if (level > 1) {
+			unfrequentFunctionLevel2(level);
+		}
+	}
+
+	void unusedFunction() {
+	}
+
+	void frequentFunction() {
+		// does nothing
+	}
+
+	int main() {
+		unfrequentFunctionLevel1(4);
+
+		for (int i = 0; i < 1000; ++i) {
+			frequentFunction();
+		}
+		return 0;
+	}
+
+}
